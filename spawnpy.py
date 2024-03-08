b08764e56a08701db8c8635edac919d1be65d8b7
Daniel Fisher <42785665+fsher1282@users.noreply.github.com>
2024-03-08 11:04:14 -0800

    Update spawnpy.py

--- spawnpy.py
+++ spawnpy.py
@@ -1,37 +1,36 @@
 #!/usr/bin/env python3
 import argparse
-import sys
-
-
-def main(args):
-    # Check if verbose mode is enabled
-    if args.verbose:
-        print("Verbose mode enabled.")
-
-    # Placeholder for handling the custom input argument
-    if args.custom_input is not None:
-        print(f"Custom input provided: {args.custom_input}")
-    else:
-        print("No args provided.")
-
-    # Your script's main logic here
-    # ...
 
 
 if __name__ == "__main__":
     # Create the parser
-    # Replace the description for your project needs
-    parser = argparse.ArgumentParser(description="A generic Python script template for Linux shell commands.")
+    parser = argparse.ArgumentParser(description="Generates a generic Python script for Linux shell commands.")
 
-    # Add or remove arguments as needed
-    parser.add_argument("-V", "--verbose", help="Increase output verbosity.", action="store_true")
-
-    parser.add_argument("-c", "--custom-input",
-                        help="Edit to fit project", type=str)
+    # Get Script name
+    parser.add_argument("-n", "--script-name", help="Name of the generated Python script file.")
 
     # Parse the command line arguments
     args = parser.parse_args()
 
-    # Pass the arguments to the main function
-    main(args)
+    # Handle case where user adds .py extension
+    if args.script_name[-3:] == '.py':
+        script_name = args.script_name
+    else:
+        script_name = args.script_name + '.py'
+
+    try:
+        with open('template.py', 'r') as template_file:  # Open template file
+            contents = template_file.readlines()
+
+        with open(script_name, 'x') as new_file:  # Create new file and write contents
+            for content in contents:
+                new_file.write(content)
+
+        print(f"Script {script_name} was created successfully.")
+
+    except FileExistsError:
+        print(f"Error: The file '{script_name}' already exists.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
 
